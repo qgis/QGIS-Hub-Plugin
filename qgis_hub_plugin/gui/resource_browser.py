736e9b40367083ee6123c0855b2316a9f481ac77
Ismail Sunni <imajimatika@gmail.com>
2023-04-21 12:26:42 +0200

    Better interface to show the list of resource.

--- qgis_hub_plugin/gui/resource_browser.py
+++ qgis_hub_plugin/gui/resource_browser.py
@@ -1,7 +1,13 @@
 import os
 
 from qgis.PyQt import uic
+from qgis.PyQt.QtCore import QSize
+from qgis.PyQt.QtGui import QStandardItem, QStandardItemModel
 from qgis.PyQt.QtWidgets import QDialog
+
+from qgis_hub_plugin.core.api_client import get_all_resources
+from qgis_hub_plugin.toolbelt import PlgLogger
+from qgis_hub_plugin.utilities.common import get_icon
 
 UI_CLASS = uic.loadUiType(
     os.path.join(os.path.dirname(__file__), "resource_browser.ui")
@@ -14,3 +20,40 @@ class ResourceBrowserDialog(QDialog, UI_CLASS):
         self.setupUi(self)
         self.parent = parent
         self.iface = iface
+        self.log = PlgLogger().log
+
+        # Resources
+        self.resource_model = QStandardItemModel(self.listViewResources)
+        self.listViewResources.setModel(self.resource_model)
+        # TODO(IS): make user able to change the icon size
+        self.listViewResources.setIconSize(QSize(96, 96))
+
+        # Load resource for the first time
+        self.populate_resources()
+
+    def populate_resources(self):
+        response = get_all_resources()
+        total = response.get("total")
+        previous_url = response.get("previous")
+        next_url = response.get("next")
+        resources = response.get("results", {})
+        self.log(f"{total}, {previous_url}, {next_url}")
+
+        for resource in resources:
+            item = _create_resource_item(resource)
+            self.resource_model.appendRow(item)
+
+
+def shorten_string(text: str) -> str:
+    if len(text) > 20:
+        text = text[:17] + "..."
+    return text
+
+
+def _create_resource_item(params: dict):
+    name = shorten_string(params.get("name"))
+    item = QStandardItem(name)
+    item.setToolTip(f'{params.get("name")} by {params.get("creator")}')
+    # TODO(IS): Use different icon for different resource type or use the preview
+    item.setIcon(get_icon("qbrowser_icon.svg"))
+    return item
