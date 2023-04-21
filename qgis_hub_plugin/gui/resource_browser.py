1fe3fa9dace8c411307d03c201d48842f9a2c921
Ismail Sunni <imajimatika@gmail.com>
2023-04-21 18:04:58 +0200

    Add image preview, but blurry for some reason.

--- qgis_hub_plugin/gui/resource_browser.py
+++ qgis_hub_plugin/gui/resource_browser.py
@@ -1,13 +1,14 @@
 import os
 
+import requests
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import QSize, Qt
-from qgis.PyQt.QtGui import QStandardItem, QStandardItemModel
+from qgis.PyQt.QtCore import QSize, Qt, pyqtSlot
+from qgis.PyQt.QtGui import QPixmap, QStandardItem, QStandardItemModel
 from qgis.PyQt.QtWidgets import QDialog
 
 from qgis_hub_plugin.core.api_client import get_all_resources
 from qgis_hub_plugin.toolbelt import PlgLogger
-from qgis_hub_plugin.utilities.common import get_icon
+from qgis_hub_plugin.utilities.common import download_image, get_icon
 
 UI_CLASS = uic.loadUiType(
     os.path.join(os.path.dirname(__file__), "resource_browser.ui")
@@ -25,6 +26,11 @@ class ResourceBrowserDialog(QDialog, UI_CLASS):
         # Resources
         self.resource_model = QStandardItemModel(self.listViewResources)
         self.listViewResources.setModel(self.resource_model)
+
+        self.resource_selection_model = self.listViewResources.selectionModel()
+        self.resource_selection_model.selectionChanged.connect(
+            self.on_resource_selecton_changed
+        )
         # TODO(IS): make user able to change the icon size
         self.listViewResources.setIconSize(QSize(96, 96))
 
@@ -42,6 +48,43 @@ class ResourceBrowserDialog(QDialog, UI_CLASS):
         for resource in resources:
             item = ResourceItem(resource)
             self.resource_model.appendRow(item)
+
+    @pyqtSlot("QItemSelection", "QItemSelection")
+    def on_resource_selecton_changed(self, selected, deselected):
+        if self.selected_resource():
+            selected_resource = self.selected_resource()
+            self.update_preview()
+            self.log(f"selected resource: {selected_resource.name}")
+        else:
+            # No construction phase is selected thus remove all selection of main cable
+            self.log("no resource selected")
+
+    def selected_resource(self):
+        selected_indexes = self.listViewResources.selectionModel().selectedIndexes()
+        if len(selected_indexes) > 0:
+            return self.resource_model.itemFromIndex(selected_indexes[0])
+
+    def update_preview(self):
+        selected_resource = self.selected_resource()
+        # Thumbnail
+        # TODO: save it as a file (caching)
+        image_path = f"/home/ismailsunni/Downloads/{selected_resource.uuid}.{selected_resource.thumbnail.split('.')[-1]}"
+        self.log(image_path)
+
+        if not os.path.exists(image_path):
+            download_image(
+                selected_resource.thumbnail,
+                image_path,
+            )
+
+        # data = requests.get(selected_resource.thumbnail)
+        pixmap = QPixmap()
+        pixmap.load(image_path)
+        pixmap.scaled(256, 256, Qt.KeepAspectRatio)
+        # pixmap.loadFromData(data.content)
+        self.labelIResourcePreview.setPixmap(pixmap)
+
+        # Description
 
 
 def shorten_string(text: str) -> str:
--- qgis_hub_plugin/utilities/common.py
+++ qgis_hub_plugin/utilities/common.py
@@ -1,5 +1,7 @@
 import os
+import shutil  # save img locally
 
+import requests  # request img from web
 from qgis.PyQt.QtGui import QIcon
 
 from qgis_hub_plugin.__about__ import DIR_PLUGIN_ROOT
@@ -8,3 +10,17 @@ from qgis_hub_plugin.__about__ import DIR_PLUGIN_ROOT
 def get_icon(icon_name: str):
     full_path = os.path.join(DIR_PLUGIN_ROOT, "resources", "images", icon_name)
     return QIcon(full_path)
+
+
+def download_image(url: str, file_path: str):
+    response = requests.get(url, stream=True)
+    with open(file_path, "wb") as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+
+    # if response.status_code == 200:
+    #     with open(file_path, "wb") as f:
+    #         shutil.copyfileobj(response.raw, f)
+    #         print("Image sucessfully Downloaded: ", file_path)
+    # else:
+    #     print("Image Couldn't be retrieved")
