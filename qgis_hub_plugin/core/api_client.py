5cc400b2830c27e617cfff56001bbe87632bebf0
Ismail Sunni <imajimatika@gmail.com>
2023-07-12 10:42:49 +0700

    Do not use request.

--- qgis_hub_plugin/core/api_client.py
+++ qgis_hub_plugin/core/api_client.py
@@ -1,35 +1,28 @@
 import json
 from pathlib import Path
 
-import requests
 from qgis.core import QgsApplication
+
+from qgis_hub_plugin.utilities.common import download_file
 
 BASE_URL = "https://plugins.qgis.org/api/v1/resources/"
 
 
-def get_all_resources(params=None, force_update=False):
-    if params is None:
-        params = {}
-    params["format"] = "json"
-    params["limit"] = 1000  # hardcoded to get all resource, currently only ~160
-
+def get_all_resources(force_update=False):
     # Check if the response file exits
     response_folder = Path(QgsApplication.qgisSettingsDirPath(), "qgis_hub")
+    response_folder.mkdir(parents=True, exist_ok=True)
     response_file = Path(response_folder, "response.json")
     if not force_update and Path.exists(response_file):
         with open(response_file) as f:
             return json.load(f)
 
     # TODO: download in the background
-    response = requests.get(BASE_URL, params=params)
+    # hardcoded to get all resource, currently only ~160
+    url = f"{BASE_URL}?limit=1000&format=json"
 
-    if response.status_code == 200:
-        # Store the response in a file
-        response_folder.mkdir(parents=True, exist_ok=True)
-        with open(response_file, "w") as f:
-            json.dump(response.json(), f)
-
-        return response.json()
-
+    if download_file(url=url, file_path=response_file, force=force_update):
+        with open(response_file) as f:
+            return json.load(f)
     else:
         return None
